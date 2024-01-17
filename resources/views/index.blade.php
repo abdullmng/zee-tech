958bbf861bdd91a4bd096ef9ef15c2e96c37be3a
Muhammad Abdullahi <abdullmng@gmail.com>
2024-01-17 11:42:17 +0100

    fix address, email and hamburger menu

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,7 +19,7 @@
       <nav class="navbar navbar-expand-lg center-logo transparent navbar-light">
         <div class="container flex-lg-row flex-nowrap align-items-center">
           <div class="navbar-brand w-100">
-            <a href="/index.html">
+            <a href="/">
               {{-- <imgsrc="/assets/img/logo.png"srcset="/assets/img/logo@2x.png2x"alt=""/> --}}
               <h4 class="Text-dark">Zee-<span class="text-primary">Tech</span></h4>
             </a>
@@ -40,8 +40,8 @@
               <!-- /.navbar-nav -->
               <div class="offcanvas-footer d-lg-none">
                 <div>
-                  <a href="mailto:info@zee-tech.ng" class="link-inverse">info@zee-tech.ng</a>
-                  <br /> 00 (123) 456 78 90 <br />
+                  <a href="mailto:info@zeetech.ng" class="link-inverse">info@zeetech.ng</a>
+                  <br /> 08096460055 <br />
                   <nav class="nav social social-white mt-4">
                     <a href="#"><i class="uil uil-twitter"></i></a>
                     <a href="#"><i class="uil uil-facebook-f"></i></a>
@@ -58,8 +58,8 @@
           </div>
           <!-- /.navbar-collapse -->
           <div class="navbar-other w-100 d-flex ms-auto">
-            {{--<ul class="navbar-nav flex-row align-items-center ms-auto">
-              <li class="nav-item dropdown language-select text-uppercase">
+            <ul class="navbar-nav flex-row align-items-center ms-auto">
+              {{--<li class="nav-item dropdown language-select text-uppercase">
                 <a class="nav-link dropdown-item dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">En</a>
                 <ul class="dropdown-menu">
                   <li class="nav-item"><a class="dropdown-item" href="#">En</a></li>
@@ -69,11 +69,11 @@
               </li>
               <li class="nav-item d-none d-md-block">
                 <a href="/contact.html" class="btn btn-sm btn-primary rounded-pill">Contact</a>
-              </li>
+              </li>--}}
               <li class="nav-item d-lg-none">
                 <button class="hamburger offcanvas-nav-btn"><span></span></button>
               </li>
-            </ul>--}}
+            </ul>
             <!-- /.navbar-nav -->
           </div>
           <!-- /.navbar-other -->
@@ -448,7 +448,7 @@
                 </div>
                 <div>
                   <h5 class="mb-1">Address</h5>
-                  <address>Moonshine St. 14/05 Light City, London</address>
+                  <address>Suite 03 & 04 First Floor aminasia trade center, Aminu Kano crescent Wuse 2, Abuja</address>
                 </div>
               </div>
               <div class="d-flex flex-row">
@@ -457,7 +457,7 @@
                 </div>
                 <div>
                   <h5 class="mb-1">Phone</h5>
-                  <p>00 (123) 456 78 90</p>
+                  <p>08096460055</p>
                 </div>
               </div>
               <div class="d-flex flex-row">
@@ -466,7 +466,7 @@
                 </div>
                 <div>
                   <h5 class="mb-1">E-mail</h5>
-                  <p class="mb-0"><a href="mailto:info@zee-tech.ng" class="link-body">info@zee-tech.ng</a></p>
+                  <p class="mb-0"><a href="mailto:info@zeetech.ng" class="link-body">info@zeetech.ng</a></p>
                 </div>
               </div>
             </div>
@@ -503,8 +503,8 @@
         <div class="col-md-4 col-lg-3">
           <div class="widget">
             <h4 class="widget-title text-white mb-3">Get in Touch</h4>
-            <address class="pe-xl-15 pe-xxl-17">Moonshine St. 14/05 Light City, London, United Kingdom</address>
-            <a href="mailto:#">info@zee-tech.ng</a><br /> 00 (123) 456 78 90
+            <address class="pe-xl-15 pe-xxl-17">Suite 03 & 04 First Floor aminasia trade center, Aminu Kano crescent Wuse 2, Abuja</address>
+            <a href="mailto:info@zeetech.ng">info@zeetech.ng</a><br />08096460055
           </div>
           <!-- /.widget -->
         </div>
